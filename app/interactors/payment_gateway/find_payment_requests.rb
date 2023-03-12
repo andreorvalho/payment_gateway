439285bc6c305c67a616f10db50696d91600cbaf
andre.orvalho <andre.orvalho@gmail.com>
2023-03-12 16:13:32 +0000

    added request to customer facade

--- app/interactors/payment_gateway/find_payment_requests.rb
+++ app/interactors/payment_gateway/find_payment_requests.rb
@@ -1,7 +1,11 @@
 module PaymentGateway
   class FindPaymentRequests
     def call
-      PaymentRequest.all.to_a
+      PaymentRequest.all.to_a.inject({}) do |response_hash, payment_request|
+        facade = ::CustomerData::CustomerFacade.new(payment_request.customer_id)
+        customer_information = facade.information_for_payments
+        response_hash[payment_request.id] = payment_request.as_json.merge(customer_information)
+      end
     end
   end
 end
--- spec/controllers/payment_gateway/payment_requests_controller_spec.rb
+++ spec/controllers/payment_gateway/payment_requests_controller_spec.rb
@@ -3,10 +3,22 @@ require 'rails_helper'
 RSpec.describe PaymentGateway::PaymentRequestsController, type: :controller do
   describe 'GET index' do
     let!(:request) { create(:payment_request) }
+    let(:customer_data) { { name: 'Andre', place_of_birth: "Portugal", date_of_birth: Time.now - 36.years } }
 
     it 'returns a successful response' do
+      expect_any_instance_of(::CustomerData::CustomerFacade)
+        .to receive(:information_for_payments)
+        .and_return(customer_data)
+
       get :index
-      expect(@controller.view_assigns["payments"]).to include(request)
+      customer_data.keys.each do |key|
+        expect(@controller.view_assigns["payments"].keys).to include(key)
+      end
+
+      request.as_json.keys.each do |key|
+        expect(@controller.view_assigns["payments"].keys).to include(key)
+      end
+
       expect(response).to be_successful
     end
   end
