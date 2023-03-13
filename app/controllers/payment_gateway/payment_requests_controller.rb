48147f7a401d70764ecc368dc60eea897d2ebcde
andre.orvalho <andre.orvalho@gmail.com>
2023-03-13 16:22:23 +0000

    added response

--- app/controllers/payment_gateway/payment_requests_controller.rb
+++ app/controllers/payment_gateway/payment_requests_controller.rb
@@ -2,6 +2,7 @@ module PaymentGateway
   class PaymentRequestsController < ApplicationController
     def index
       @payments = FindPaymentRequests.new.call
+      render json: { payments: @payments }
     end
   end
 end
