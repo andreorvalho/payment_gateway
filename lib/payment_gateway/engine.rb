bc4578a87e9c747b6f1b2704561dbd12c25da951
andre.orvalho <andre.orvalho@gmail.com>
2023-03-13 15:00:15 +0000

    added migrations path for exportation

--- lib/payment_gateway/engine.rb
+++ lib/payment_gateway/engine.rb
@@ -5,5 +5,7 @@ module PaymentGateway
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    config.paths["db/migrate"].unshift(self.root.to_s + "/db/payment_gateway_migrate")
   end
 end
